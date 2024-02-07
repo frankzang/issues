fb601bae2ec5f31a29450a0d343c49ce74913dcf
Francisco Santos <fsantos.fs37@gmail.com>
2024-02-07 19:37:40 -0300

    initial commit

---
+++ lib/issues.ex
@@ -0,0 +1,19 @@
+defmodule Issues do
+  @moduledoc """
+  Documentation for `Issues`.
+  """
+
+  @spec hello() :: :world
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Issues.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
